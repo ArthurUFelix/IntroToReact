2b71f4c5e864ce552dd65b9a39f5f2eb594cdbd2
Arthur Felix <arthurufelix@gmail.com>
2020-03-01 10:23:31 -0300

    escrever primeiro código React, sem JSX

---
+++ src/App.js
@@ -0,0 +1,26 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Panda",
+      animal: "Dog",
+      breed: "Shietzu"
+    }),
+    React.createElement(Pet, { name: "Ursa", animal: "Dog", breed: "Shietzu" }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel"
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
