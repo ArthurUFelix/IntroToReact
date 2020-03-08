2ab5328f0b75b683656c27a22bc8f2d1122cef1e
Arthur Felix <arthurufelix@gmail.com>
2020-03-08 11:20:09 -0300

    Chamada assíncrona para Petfinder API, atualizar componente Pet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Panda",
-      animal: "Dog",
-      breed: "Shietzu"
-    }),
-    React.createElement(Pet, { name: "Ursa", animal: "Dog", breed: "Shietzu" }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    })
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
